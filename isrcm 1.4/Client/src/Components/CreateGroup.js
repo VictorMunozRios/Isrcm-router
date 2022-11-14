dcd01493d1b3ae54eb4116f6d01630fe03a221db
VictorMunozRios <victor.munoz.rios@alumnos.uta.cl>
2022-11-14 17:07:23 -0300

    sprint 2 casi listo

---
+++ isrcm 1.4/Client/src/Components/CreateGroup.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import CrearGrupFile from './CrearGrupFile'
+
+function CreateGroup() {
+  return (
+    <div className="CreateGroup">
+        
+        <CrearGrupFile/>
+    </div>
+  )
+}
+
+export default CreateGroup
+
+
+{/* <ActiButton/>    
+<BasicSelect/>
+<b1>Empleados</b1>
+<CheckAñadir/>
+<b1>Empleados que harán curso de "____"</b1>
+<CheckEliminar/> */}
